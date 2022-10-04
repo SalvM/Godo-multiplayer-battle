d9a7af52b1efc4c98d2ad99fb005be98485335a1
SalvM <salvm93@gmail.com>
2022-10-04 21:18:07 +0200

    Lobby system

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,7 +1,47 @@
 extends Control
 
-func _ready():
-	pass
+signal log_status(new_status)
 
+onready var statusLabel = $Status
+onready var roomPopup = $CenterContainer/RoomPopup
+onready var players = $CenterContainer/RoomPopup/VBoxContainer/Players
+onready var youLabel = $CenterContainer/RoomPopup/VBoxContainer/Players/You
+
+func clear_players_label():
+	for labels in players.get_children():
+		if labels.name != "You":
+			labels.queue_free()
+
+func _ready():
+	Server.connect("log_status", self, "_on_new_log_status")
+	Server.connect("refresh_room_status", self, "_on_room_status_refresh")
+
+func _on_new_log_status(new_status: String):
+	statusLabel.text = new_status
+
+func _on_room_status_refresh(new_status):
+	var new_players = new_status.players
+	youLabel.visible = false
+	
+	clear_players_label()
+	
+	for player_id in new_players:
+		var new_label = youLabel.duplicate()
+		new_label.set_name(str(player_id))
+		new_label.text = "#" + str(player_id)
+		if player_id == get_tree().get_network_unique_id():
+			new_label.text += " (You)"
+		new_label.visible = true
+		players.add_child(new_label)
+	
 func _on_JoinBtn_pressed():
 	Server.connect_to_server()
+	roomPopup.show()
+
+func _on_ExitBtn_pressed():
+	Server.disconnect_from_server()
+	roomPopup.hide()
+	clear_players_label()
+
+func _on_JoinRoomBtn_pressed():
+	Server.fetch_user_join_room()
--- scripts/Server.gd
+++ scripts/Server.gd
@@ -5,19 +5,38 @@ onready var Puppet = preload("res://scenes/Knight.tscn")
 var network = NetworkedMultiplayerENet.new()
 var ip = "127.0.0.1"
 var port = 1909
-var max_players = 4
+var match_room = {
+	"status": "waiting",
+	"players": {}
+}
+
+signal log_status(new_status)
+signal refresh_room_status(new_status)
 
 func connect_to_server():
+	var connection_server = ip + ":" + str(port)
+	emit_signal("log_status", "Connecting to " + connection_server + " ...")
 	network.create_client(ip, port)
 	get_tree().set_network_peer(network)
 	printt("Connected at port", port)
-	
+	emit_signal("log_status", "Successfully connected!")
+
 	network.connect("peer_connected", self, "_on_peer_connected")
 	network.connect("peer_disconnected", self, "_on_peer_disconnected")
 
 func disconnect_from_server():
+	network.close_connection()
 	get_tree().network_peer = null
+	emit_signal("log_status", "Disconnected from the server")	
 
+remote func return_room_status(room_status):
+	printt("return_room_status", room_status)
+	match_room = room_status
+	emit_signal("refresh_room_status", room_status)
+
+remote func fetch_user_join_room():
+	rpc_id(1, "fetch_user_join_room")
+	
 remote func user_load_battlefield(peer_id):
 	printt('user_load_battlefield', peer_id)
 	get_tree().change_scene("res://scenes/BattleField.tscn")
@@ -47,6 +66,8 @@ func _ready():
 
 func _on_peer_connected(peer_id):
 	print("User #" + str(peer_id) + " connected")
-	
+
 func _on_peer_disconnected(peer_id):
 	print("User #" + str(peer_id) + " disconnected")
+	if peer_id == get_tree().get_network_unique_id():
+		emit_signal("log_status", "Disconnected from the server")
